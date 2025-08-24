ccc8585c50e28ff69f84015fa4c1951ea0a6f468
praveen <kpking900@gmail.com>
2025-08-24 12:21:51 +0530

    new

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+// next.config.js or next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
